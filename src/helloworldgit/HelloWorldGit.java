2788b9c2727ae5654e288647c6535532da564d1a
ppesq <betoesquivel>
2014-02-24 14:07:22 -0600

    Agregué mi nombre en el comentario al main. Porque hugo resolvio conflictos de merge borrandolo.

--- src/helloworldgit/HelloWorldGit.java
+++ src/helloworldgit/HelloWorldGit.java
@@ -21,7 +21,8 @@ public class HelloWorldGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //Hugo León Garza
+        //José Alberto Esquivel A01139626   
+        //Hugo León Garza A01139720
         JFrameHolaMundo holaMundo = new JFrameHolaMundo();
         holaMundo.setVisible(true);
     }
