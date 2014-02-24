b273bc7c7ef5fb39c5258f03ab455488eda76760
ppesq <betoesquivel>
2014-02-24 12:52:17 -0600

    First commit... Ya funciona el hello world.

---
+++ src/helloworldgit/HelloWorldGit.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworldgit;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author ppesq
+ */
+public class HelloWorldGit {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrameHolaMundo holaMundo = new JFrameHolaMundo();
+        holaMundo.setVisible(true);
+    }
+
+}
